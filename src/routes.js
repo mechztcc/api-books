4546f9dea42ed148f370931de71f9bb42fc8f21b
mechztcc <dojathan@gmail.com>
2021-07-26 16:40:31 -0300

    Added Celebrate to handle with the JSON req

--- src/routes.js
+++ src/routes.js
@@ -3,15 +3,44 @@ const UserController = require('./controllers/UserController');
 const BookController = require('./controllers/BookController');
 const AuthController = require('./controllers/AuthController');
 const authMiddleware = require('./middlewares/auth');
+const { celebrate, Segments, Joi } = require('celebrate');
 
 const routes = express.Router();
 
-routes.post('/auth', AuthController.auth);
+routes.post(
+  '/auth',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  AuthController.auth
+);
 
-routes.post('/users', UserController.store);
+routes.post(
+  '/users',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  UserController.store
+);
 routes.get('/users', UserController.index);
 
-routes.post('/users/books', authMiddleware, BookController.store);
+routes.post(
+  '/users/books',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+    },
+  }),
+  authMiddleware,
+  BookController.store
+);
 routes.get('/users/books', authMiddleware, BookController.index);
 routes.delete('/users/books/:book_id', authMiddleware, BookController.delete);
 
--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
-const express = require("express");
-const routes = require("./routes");
-require("./database");
+const { Segments, errors } = require('celebrate');
+const express = require('express');
+const routes = require('./routes');
+
+require('./database');
 
 const app = express();
 app.use(express.json());
 app.use(routes);
+// celebrate error handler
+app.use(errors());
 
 app.listen(3000, () => {
-  console.log("Server running at localhost:3000");
+  console.log('Server running at localhost:3000');
 });
